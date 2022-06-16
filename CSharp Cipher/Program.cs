d73df3b8aa9afa26f8196f95cb1b53ae23f3cf1b
Seth Villalon <sethvillalon95@gmail.com>
2022-06-16 10:35:31 -1000

    Test .gitignore

---
+++ CSharp Cipher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharp_Cipher
+{
+    class Program
+    {
+
+        public Program()
+        {
+            Say("Welcome! Please type in the console the text to encrypt: ");
+            string input = Console.ReadLine();
+
+        }
+
+        public static void Say(Object o)
+        {
+            Console.WriteLine(o);
+        }
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+    }
+}
